188a6e7a5c7301e818e18858b4e6804d91470e20
novosadnazar <163895331+novosadnazar@users.noreply.github.com>
2025-02-09 13:17:49 +0200

    jes

---
+++ js/script.js
@@ -0,0 +1,25 @@
+// 1Створити змінні для зберігання температури за Цельсієм та Фаренгейтом. Перевести температуру з Цельсія у Фаренгейти за допомогою формули та вивести результат в консоль.
+
+const celsius = 30;
+const result = celsius * 9 / 5 + 32;
+console.log(result);
+
+// 2Створити змінну для зберігання кількості днів у місяці. Обчислити кількість годин та хвилин у цьому місяці за допомогою оператора множення та вивести результат в консоль.
+
+// хвилини
+const storageDays = 30;
+const resultTwo = storageDays * 24; 
+console.log(resultTwo);
+
+const storageMin = 720;
+const resultThree = storageMin * 60;
+console.log(resultThree);
+
+// Створити змінні для зберігання рівня здоров'я та енергії гравця в грі. Зменшити рівень здоров'я та енергії гравця за допомогою операторів віднімання та вивести результат в консоль.
+
+const saveHelth = 20;
+const saveEnergy = 50;
+const resultHelth = saveHelth - 5;
+console.log(resultHelth);
+const resultEnergy = saveEnergy - 20;
+console.log(resultEnergy);
