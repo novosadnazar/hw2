09ec1e6da70b5dc8c517cb7abddef79f5088bd75
novosadnazar <163895331+novosadnazar@users.noreply.github.com>
2025-02-12 18:13:54 +0200

    fullll

--- js/script.js
+++ js/script.js
@@ -1,30 +1,30 @@
 // 1Створити змінні для зберігання температури за Цельсієм та Фаренгейтом. Перевести температуру з Цельсія у Фаренгейти за допомогою формули та вивести результат в консоль.
 
-const celsius = 30;
-const result = celsius * 9 / 5 + 32;
-console.log(result);
+// const celsius = 30;
+// const result = celsius * 9 / 5 + 32;
+// console.log(result);
 
 // 2Створити змінну для зберігання кількості днів у місяці. Обчислити кількість годин та хвилин у цьому місяці за допомогою оператора множення та вивести результат в консоль.
 
 // хвилини
-const storageDays = 30;
-const resultTwo = storageDays * 24; 
-console.log(resultTwo);
+// const storageDays = 30;
+// const resultTwo = storageDays * 24; 
+// console.log(resultTwo);
 
-const resultThree = resultTwo * 60;
-console.log(resultTwo);
+// const resultThree = resultTwo * 60;
+// console.log(resultTwo);
 
 // Створити змінні для зберігання рівня здоров'я та енергії гравця в грі. Зменшити рівень здоров'я та енергії гравця за допомогою операторів віднімання та вивести результат в консоль.
 
-const saveHelth = 20;
-const saveEnergy = 50;
-const resultHelth = saveHelth - 5;
-console.log(resultHelth);
-const resultEnergy = saveEnergy - 20;
-console.log(resultEnergy);
+// const saveHelth = 20;
+// const saveEnergy = 50;
+// const resultHelth = saveHelth - 5;
+// console.log(resultHelth);
+// const resultEnergy = saveEnergy - 20;
+// console.log(resultEnergy);
 
 // Створити змінну для зберігання суми покупки в магазині. Застосувати знижку в розмірі 10% до цієї суми за допомогою оператора множення та вивести результат в консоль.
 
-const storageSum = 100;
-const resultSum = storageSum * 0.10;
-console.log(resultSum);
+// const storageSum = 100;
+// const resultSum = storageSum * 0.10;
+// console.log(resultSum);
---
+++ js/scriptTwo.js
@@ -0,0 +1,18 @@
+const coma = 123.7658;
+console.log(Math.floor(coma));
+
+const number = 3.14;
+console.log(Number(parseFloat(number)));
+
+const widthElement = "5.90";
+const widthEl = Number(parseInt(widthElement))
+console.log(widthEl);
+
+const rootSquare = 60;
+console.log(Math.sqrt(rootSquare));
+
+const seven = 256;
+const sevenTh = "456";
+const result = Number(parseInt(sevenTh))
+console.log(result);
+console.log(seven.toString());
